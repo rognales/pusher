538f045f1ec8f5b5df5717510b4390bbd38eea7d
rognales <rognales@gmail.com>
2017-08-09 13:50:54 +0800

    adas

--- app/Events/SendMessage.php
+++ app/Events/SendMessage.php
@@ -21,7 +21,7 @@ class SendMessage implements ShouldBroadcast
      */
     public function __construct($message)
     {
-        $this->message = $message;
+        $this->message = "Hellow brother";
     }
 
     /**
